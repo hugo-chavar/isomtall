147963ccd8c937b366185ce46b4dea11a661e7c3
Imperoris <Imperoris@gmail.com>
2013-05-06 22:41:20 +0000

    Prototipo para agregar el nombre.

--- View/Personaje.h
+++ View/Personaje.h
@@ -36,8 +36,10 @@ private:
 	void freezar();
 	void animar();
 	void detenerAnimacion();
+	void crearNombre();
 
 	std::pair<int, int> tileActual;
+	/*SDL_Surface *nombre;*/
 	PersonajeModelo* modelo;
 	std::vector<SpriteAnimado*> sprites;
 	float velocidad; //velocidad actual
