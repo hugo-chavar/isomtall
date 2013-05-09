42cc5cffb12474c42a932b5c82cbba0209456735
hechavar <hechavar@gmail.com>
2013-05-09 05:26:02 +0000

    Vision del personaje es una elipse, como lo dijeron en clase.

--- Model/CharacterVision.cpp
+++ Model/CharacterVision.cpp
@@ -24,31 +24,38 @@ void CharacterVision::initialize(){
 		mapKnowledge.push_back(line);
 	}
 	this->setKnown(this->position);
+	vision.initialize(this->position, this->rangeVision);
+	vision.fill();
 	pair<int, int > aux;
-	for (int i = 1; i <= this->rangeVision; i++){
-		aux = this->position;
-		for (int j = i; j > 0; j--){
-			aux.second = this->position.second - j;
-			aux.first = this->position.first - (i - j);
-			this->setKnown(aux);
-			if (this->position.first != aux.first){
-				aux.first = this->position.first + (i - j);
-				this->setKnown(aux);
-			}
-			aux.second = this->position.second + j;
-			aux.first = this->position.first - (i - j);
-			this->setKnown(aux);
-			if (this->position.first != aux.first){
-				aux.first = this->position.first + (i - j);
-				this->setKnown(aux);
-			}
-		}
-		aux.second = this->position.second;
-		aux.first = this->position.first - i;
-		this->setKnown(aux);
-		aux.first = this->position.first + i;
+	while (vision.hasNext()) {
+		aux = vision.next();
 		this->setKnown(aux);
 	}
+	//
+	//for (int i = 1; i <= this->rangeVision; i++){
+	//	aux = this->position;
+	//	for (int j = i; j > 0; j--){
+	//		aux.second = this->position.second - j;
+	//		aux.first = this->position.first - (i - j);
+	//		this->setKnown(aux);
+	//		if (this->position.first != aux.first){
+	//			aux.first = this->position.first + (i - j);
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = this->position.second + j;
+	//		aux.first = this->position.first - (i - j);
+	//		this->setKnown(aux);
+	//		if (this->position.first != aux.first){
+	//			aux.first = this->position.first + (i - j);
+	//			this->setKnown(aux);
+	//		}
+	//	}
+	//	aux.second = this->position.second;
+	//	aux.first = this->position.first - i;
+	//	this->setKnown(aux);
+	//	aux.first = this->position.first + i;
+	//	this->setKnown(aux);
+	//}
 }
 
 void CharacterVision::setPosition(pair<int, int> pos){
@@ -58,102 +65,111 @@ void CharacterVision::setPosition(pair<int, int> pos){
 void CharacterVision::updatePosition(pair<int, int> pos){
 	if (pos == this->position)
 		return;
-	pair<int, int > aux = pos;
-	if (pos.first > this->position.first) { //TODO: eliminar codigo repetido
-		if (pos.second == this->position.second){
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = this->position.second - j;
-				aux.first = pos.first + (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second = this->position.second + j;
-				this->setKnown(aux);
-			}
-			aux.second = this->position.second;
-			aux.first = pos.first + this->rangeVision;
+	vision.clear();
+	vision.initialize(pos, this->rangeVision);
+	vision.fill();
+	pair<int, int > aux;
+	while (vision.hasNext()) {
+		aux = vision.next();
+		
 			this->setKnown(aux);
-		} else if (pos.second > this->position.second) {
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = pos.second + j;
-				aux.first = pos.first + (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second -= 1;
-				this->setKnown(aux);
-			}
-			aux.second = pos.second;
-			aux.first = pos.first + this->rangeVision;
-			this->setKnown(aux);
-		} else  { //if (pos.second < this->position.second)
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = pos.second - j;
-				aux.first = pos.first + (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second += 1;
-				this->setKnown(aux);
-			}
-			aux.second = pos.second;
-			aux.first = pos.first + this->rangeVision;
-			this->setKnown(aux);
-		}
-	} else if (pos.first < this->position.first) {
-		if (pos.second == this->position.second){
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = this->position.second - j; 
-				aux.first = pos.first - (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second = this->position.second + j;
-				this->setKnown(aux);
-			}
-			aux.second = this->position.second;
-			aux.first = pos.first - this->rangeVision;
-			this->setKnown(aux);
-		} else if (pos.second > this->position.second) { 
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = pos.second + j;
-				aux.first = pos.first - (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second -= 1;
-				this->setKnown(aux);
-			}
-			aux.second = pos.second;
-			aux.first = pos.first - this->rangeVision;
-			this->setKnown(aux);
-		} else  { //if (pos.second < this->position.second)
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.second = pos.second - j;
-				aux.first = pos.first - (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.second += 1;
-				this->setKnown(aux);
-			}
-			aux.second = pos.second;
-			aux.first = pos.first - this->rangeVision;
-			this->setKnown(aux);
-		}
-	} else {
-		if (pos.second > this->position.second) {
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.first = this->position.first - j; 
-				aux.second = pos.second + (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.first = this->position.first + j;
-				this->setKnown(aux);
-			}
-			aux.first = this->position.first; 
-			aux.second = pos.second + this->rangeVision;
-			this->setKnown(aux);
-		} else  {
-			for (int j = this->rangeVision; j > 0; j--){
-				aux.first = this->position.first - j;
-				aux.second = pos.second - (this->rangeVision - j);
-				this->setKnown(aux);
-				aux.first = this->position.first + j;
-				this->setKnown(aux);
-			}
-			aux.first = this->position.first;
-			aux.second = pos.second - this->rangeVision;
-			this->setKnown(aux);
-		}
 	}
+	//pair<int, int > aux = pos;
+	//if (pos.first > this->position.first) { //TODO: eliminar codigo repetido
+	//	if (pos.second == this->position.second){
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = this->position.second - j;
+	//			aux.first = pos.first + (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second = this->position.second + j;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = this->position.second;
+	//		aux.first = pos.first + this->rangeVision;
+	//		this->setKnown(aux);
+	//	} else if (pos.second > this->position.second) {
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = pos.second + j;
+	//			aux.first = pos.first + (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second -= 1;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = pos.second;
+	//		aux.first = pos.first + this->rangeVision;
+	//		this->setKnown(aux);
+	//	} else  { //if (pos.second < this->position.second)
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = pos.second - j;
+	//			aux.first = pos.first + (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second += 1;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = pos.second;
+	//		aux.first = pos.first + this->rangeVision;
+	//		this->setKnown(aux);
+	//	}
+	//} else if (pos.first < this->position.first) {
+	//	if (pos.second == this->position.second){
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = this->position.second - j; 
+	//			aux.first = pos.first - (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second = this->position.second + j;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = this->position.second;
+	//		aux.first = pos.first - this->rangeVision;
+	//		this->setKnown(aux);
+	//	} else if (pos.second > this->position.second) { 
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = pos.second + j;
+	//			aux.first = pos.first - (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second -= 1;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = pos.second;
+	//		aux.first = pos.first - this->rangeVision;
+	//		this->setKnown(aux);
+	//	} else  { //if (pos.second < this->position.second)
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.second = pos.second - j;
+	//			aux.first = pos.first - (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.second += 1;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.second = pos.second;
+	//		aux.first = pos.first - this->rangeVision;
+	//		this->setKnown(aux);
+	//	}
+	//} else {
+	//	if (pos.second > this->position.second) {
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.first = this->position.first - j; 
+	//			aux.second = pos.second + (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.first = this->position.first + j;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.first = this->position.first; 
+	//		aux.second = pos.second + this->rangeVision;
+	//		this->setKnown(aux);
+	//	} else  {
+	//		for (int j = this->rangeVision; j > 0; j--){
+	//			aux.first = this->position.first - j;
+	//			aux.second = pos.second - (this->rangeVision - j);
+	//			this->setKnown(aux);
+	//			aux.first = this->position.first + j;
+	//			this->setKnown(aux);
+	//		}
+	//		aux.first = this->position.first;
+	//		aux.second = pos.second - this->rangeVision;
+	//		this->setKnown(aux);
+	//	}
+	//}
 	this->position = pos;
 }
 
@@ -163,6 +179,8 @@ bool CharacterVision::testPosition(pair<int, int> pos){
 
 void CharacterVision::setKnown(pair<int, int> pos){
 	if (Game::instance().world()->isInsideWorld(pos)){
+		if (testPosition(pos))
+			return;
 		this->mapKnowledge[pos.second].set(pos.first);
 		TileModel* relatedTile = Game::instance().world()->getTileAt(pos)->getRelatedTile();
 		if (relatedTile){
@@ -176,8 +194,9 @@ void CharacterVision::setKnown(pair<int, int> pos){
 }
 
 bool CharacterVision::isInsideVision(pair<int, int> pos){
-
-	int bottom =  (this->position.second - this->rangeVision + abs(this->position.first - pos.first));
+	bool res = vision.inside(pos);
+	return res;
+	/*int bottom =  (this->position.second - this->rangeVision + abs(this->position.first - pos.first));
 	int top = this->position.second + this->rangeVision- abs(this->position.first - pos.first);
-	return ((pos.second  >= bottom)	&& (pos.second <= top));
+	return ((pos.second  >= bottom)	&& (pos.second <= top));*/
 }
