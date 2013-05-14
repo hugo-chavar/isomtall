7b3de9cc6fb8075eb133b394ddc574a4a5a14d8f
hechavar@gmail.com <hechavar@gmail.com@c30aaea6-1c3d-94c6-72ef-2e3a156daed0>
2013-05-14 03:15:24 +0000

    Vision to string. El final de la clase Frame.

--- trunk/View/Sprite.h
+++ trunk/View/Sprite.h
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <vector>
 #include <list>
-#include "Frame.h"
 #include "Surface.h"
 #include "AnimatedEntity.h"
 
@@ -17,34 +16,16 @@ class Sprite {
 
 protected:
 
-	int relx,rely,_baseWidth,_baseHeight; //Posicion relativa del Tile de referencia de la base respecto imagen
-	//float delay;
-	//float fps;
-	//unsigned nroFrames;
+	int relx,rely,_baseWidth,_baseHeight;
 	EntityObject* spriteEntity;
-
 	unsigned estado; 
-	//std::vector<Frame*> frames;
 	std::vector<view::Surface* > surfaces;
-	//int comienzo_frame;
-
-	//void cargarFrames(std::string path,std::string nombre,std::string formato,int nro);
-	//void cargarFrame(EntityObject* entity);
-	//virtual void loadSurfaces(EntityObject* entity);
 	virtual void loadSurfaces();
-	//void cargarFrames(DirList *imagesPaths);
-	//void inicializar(int refX,int refY,int baseWidth,int baseHeight);
 	virtual void initialize();
 
 public:
-	//Sprite(std::string path,std::string nombre,int nroFr,int relatx,int relaty);
-	//Sprite(std::string path,std::string nombre,int nroFr,int relatx,int relaty,float delay,float fps);
 	Sprite(EntityObject *entity);
 	Sprite();
-	
-	//Sprite(AnimatedEntity* entity);
-	//SDL_Rect posicionIsometrica(int posx, int posy,int centroPantallax);
-	//Frame* getFrameActual();
 	~Sprite();
 	virtual void actualizarFrame();
 	int relatx();
@@ -52,7 +33,6 @@ public:
 	int baseWidth();
 	int baseHeight();
 	unsigned getCurrentState();
-	//Frame* getFrameAt(int state);
 	view::Surface* getSurfaceAt(int state);
 	view::Surface* getCurrentSurface();
 	void addSurface(std::string path);
