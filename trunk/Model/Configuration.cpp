eb1ae0400f1e4b04c78b2e01509cd1c0528435d4
hechavar@gmail.com <hechavar@gmail.com@c30aaea6-1c3d-94c6-72ef-2e3a156daed0>
2013-05-19 16:07:09 +0000

    Cliente corrieno

--- trunk/Model/Configuration.cpp
+++ trunk/Model/Configuration.cpp
@@ -3,32 +3,32 @@
 using namespace common;
 
 Configuration::Configuration(){
-	_cameraScrollSpeed = DEFAULT_SCROLL_SPEED;
-	_cameraWidth = DEFAULT_SCREEN_WIDTH;
-	_cameraHeight = DEFAULT_SCREEN_HEIGHT;
+	this->_cameraScrollSpeed = DEFAULT_SCROLL_SPEED;
+	this->_cameraWidth = DEFAULT_SCREEN_WIDTH;
+	this->_cameraHeight = DEFAULT_SCREEN_HEIGHT;
 	//_serverPort = DEFAULT_SERVER_PORT;
 	//_serverPort = DEFAULT_SERVER_IP;
-	_visionRange = DEFAULT_VISION_RANGE;
-	_mainCharacterSpeed = DEFAULT_MAIN_CHARACTER_SPEED;
+	this->_visionRange = DEFAULT_VISION_RANGE;
+	this->_mainCharacterSpeed = DEFAULT_MAIN_CHARACTER_SPEED;
 }
 
 Configuration::Configuration(const Configuration &source){
-	_cameraScrollSpeed = source._cameraScrollSpeed;
-	_cameraWidth = source._cameraWidth;
-	_cameraHeight = source._cameraHeight;
+	this->_cameraScrollSpeed = source._cameraScrollSpeed;
+	this->_cameraWidth = source._cameraWidth;
+	this->_cameraHeight = source._cameraHeight;
 	//_serverPort = source._serverPort;
-	_visionRange = source._visionRange;
-	_mainCharacterSpeed = source._mainCharacterSpeed;
+	this->_visionRange = source._visionRange;
+	this->_mainCharacterSpeed = source._mainCharacterSpeed;
 	//la ip se setea en otro punto.. no copiarla
 }
 
 Configuration& Configuration::operator=(const Configuration &source){
-	_cameraScrollSpeed = source._cameraScrollSpeed;
-	_cameraWidth = source._cameraWidth;
-	_cameraHeight = source._cameraHeight;
+	this->_cameraScrollSpeed = source._cameraScrollSpeed;
+	this->_cameraWidth = source._cameraWidth;
+	this->_cameraHeight = source._cameraHeight;
 	//_serverPort = source._serverPort;
-	_visionRange = source._visionRange;
-	_mainCharacterSpeed = source._mainCharacterSpeed;
+	this->_visionRange = source._visionRange;
+	this->_mainCharacterSpeed = source._mainCharacterSpeed;
 	//la ip se setea en otro punto.. no copiarla
 	return *this;
 }
@@ -39,16 +39,16 @@ Configuration::~Configuration() {
 }
 
 unsigned int Configuration::cameraWidth() {
-	return _cameraWidth;
+	return this->_cameraWidth;
 }
 
 unsigned int Configuration::cameraHeight() {
-	return _cameraHeight;
+	return this->_cameraHeight;
 }
 
 void Configuration::cameraHeight(int value) {
 	if (value >= MIN_SCREEN_HEIGHT) {
-		_cameraHeight = static_cast<unsigned>(value);
+		this->_cameraHeight = static_cast<unsigned>(value);
 	} else {
 		Logger::instance().log("Game warning: Field 'alto pantalla' is too low, defaulted.");
 	}
@@ -56,30 +56,30 @@ void Configuration::cameraHeight(int value) {
 
 void Configuration::cameraWidth(int value) {
 	if (value >= MIN_SCREEN_WIDTH) {
-		_cameraWidth = static_cast<unsigned>(value);
+		this->_cameraWidth = static_cast<unsigned>(value);
 	} else {
 		Logger::instance().log("Game warning: Field 'ancho pantalla' is too low, defaulted.");
 	}
 }
 
 unsigned int Configuration::cameraMarginScroll() {
-	return _cameraMarginScroll;
+	return this->_cameraMarginScroll;
 }
 
 void Configuration::cameraMarginScroll(int value) {
 	if ((value >= MIN_SCROLL_MARGIN) && (value <= MAX_SCROLL_MARGIN)) {
-		_cameraMarginScroll = static_cast<unsigned>(value);
+		this->_cameraMarginScroll = static_cast<unsigned>(value);
 	} else if (value < MIN_SCROLL_MARGIN) {
 		Logger::instance().log("Game warning: Field 'margen_scroll' is too low, setted to minimun.");
-		_cameraMarginScroll = MIN_SCROLL_MARGIN;
+		this->_cameraMarginScroll = MIN_SCROLL_MARGIN;
 	} else if (value > MAX_SCROLL_MARGIN) {
 		Logger::instance().log("Game warning: Field 'margen_scroll' is too high, setted to maximun.");
-		_cameraMarginScroll = MAX_SCROLL_MARGIN;
+		this->_cameraMarginScroll = MAX_SCROLL_MARGIN;
 	}
 }
 
 unsigned int Configuration::cameraSpeed() {
-	return _cameraScrollSpeed;
+	return this->_cameraScrollSpeed;
 }
 
 void Configuration::initialize(int dimentionX, int dimentionY, int scr) {
@@ -91,7 +91,7 @@ void Configuration::initialize(int dimentionX, int dimentionY, int scr) {
 void Configuration::serverPort(int value) {
 	// rango de puertos validos
 	if ( (value >= 1024) && (value <= 65535) ){
-		_serverPort = static_cast<unsigned>(value);
+		this->_serverPort = static_cast<unsigned>(value);
 	} else {
 		Logger::instance().log("Game warning: Field 'puerto servidor' is not valid, defaulted.");
 		
@@ -105,7 +105,7 @@ unsigned int Configuration::serverPort() {
 void Configuration::visionRange(int value) {
 	// rango de puertos validos
 	if ( (value >= 2) && (value <= 5000) ) {
-		_visionRange = static_cast<unsigned>(value);
+		this->_visionRange = static_cast<unsigned>(value);
 	} else {
 		Logger::instance().log("Game warning: Field 'vision personaje' is not valid, defaulted.");
 		
@@ -113,29 +113,29 @@ void Configuration::visionRange(int value) {
 }
 
 float Configuration::mainCharacterSpeed() {
-	return _mainCharacterSpeed;
+	return this->_mainCharacterSpeed;
 }
 
 void Configuration::mainCharacterSpeed(float value) {
 	if ((value >= MIN_MAIN_CHARACTER_SPEED) && (value <= MAX_MAIN_CHARACTER_SPEED)) {
-		_mainCharacterSpeed = value;
+		this->_mainCharacterSpeed = value;
 	} else if (value > float(MAX_MAIN_CHARACTER_SPEED)) {
 		Logger::instance().log("Game warning: Field 'vel_personaje' is too high, setted to maximun.");
-		_mainCharacterSpeed = float(MAX_MAIN_CHARACTER_SPEED);
+		this->_mainCharacterSpeed = float(MAX_MAIN_CHARACTER_SPEED);
 	} else {
 		Logger::instance().log("Game warning: Field 'vel_personaje' is too low, setted to minimun.");
-		_mainCharacterSpeed = float(MIN_MAIN_CHARACTER_SPEED);
+		this->_mainCharacterSpeed = float(MIN_MAIN_CHARACTER_SPEED);
 	}
 }
 
 unsigned int Configuration::visionRange() {
-	return _visionRange;
+	return this->_visionRange;
 }
 
 std::string Configuration::serverIp() {
-	return _serverIp;
+	return this->_serverIp;
 }
 
 void Configuration::serverIp(std::string ip) {
-	_serverIp = ip;
+	this->_serverIp = ip;
 }
