06cbe4d3e73dbd2a56d6ba8df9d535fa709f12d4
marianowilliner <marianowilliner@gmail.com>
2013-06-03 02:58:14 +0000

    Added log to test.

--- Model/InstructionQueue.cpp
+++ Model/InstructionQueue.cpp
@@ -1,5 +1,9 @@
 #include "InstructionQueue.h"
 
+#include "Logger.h"
+#include "SDL.h"
+#include "stringUtilities.h"
+
 InstructionQueue::InstructionQueue() {
 }
 
@@ -27,20 +31,32 @@ void InstructionQueue::addInstruction(Instruction& instruction) {
 Instruction InstructionQueue::getNextInstruction(bool shouldWait) {
 	Instruction aux;
 
+	common::Logger::instance().log("getNextInstruction - 1 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 	this->getMutex().lock();
+	common::Logger::instance().log("getNextInstruction - 2 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 	if (this->getInstructionQueue().empty() && shouldWait) {
+		common::Logger::instance().log("getNextInstruction - 3 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getConditionVariable().getConditionMutex().lock();
+		common::Logger::instance().log("getNextInstruction - 4 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getMutex().unlock();
+		common::Logger::instance().log("getNextInstruction - 5 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getConditionVariable().wait();
+		common::Logger::instance().log("getNextInstruction - 6 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getMutex().lock();
+		common::Logger::instance().log("getNextInstruction - 7 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getConditionVariable().getConditionMutex().unlock();
+		common::Logger::instance().log("getNextInstruction - 8 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 	}
 
 	if (!this->getInstructionQueue().empty()) {
+		common::Logger::instance().log("getNextInstruction - 9 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		aux = this->getInstructionQueue().front();
+		common::Logger::instance().log("getNextInstruction - 10 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 		this->getInstructionQueue().pop();
+		common::Logger::instance().log("getNextInstruction - 11 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 	}
 	this->getMutex().unlock();
+	common::Logger::instance().log("getNextInstruction - 12 AT: " + stringUtilities::unsignedToString(SDL_GetTicks()));
 
 	return aux;
 }
