835750d29d7faa9f1160062ec60c5821d2a08ac3
hechavar <hechavar@gmail.com>
2013-06-14 16:49:18 +0000

    actualizo el parser para el single player

--- Model/StageModel.h
+++ Model/StageModel.h
@@ -9,6 +9,7 @@
 #include "DataTypes.h"
 #include "PersonajeModelo.h"
 #include "TileModel.h"
+#define REGENERATIONTIME 30000
 
 using namespace std;
 
@@ -17,15 +18,17 @@ class StageModel {
 
 public:
 	StageModel();
-	StageModel(string name, vector <EntityDef> vEntitiesDef, vector <PersonajeModelo*> vMainCharacters): // unsigned int width, unsigned int height,
-	_name(name), _vEntitiesDef(vEntitiesDef),  _vMainCharacters(vMainCharacters) { _tileWidth = DEFAULT_TILE_WIDTH; _tileHeight = DEFAULT_TILE_HEIGHT; firstTile = NULL; this->_tilesMap = NULL; }; //_width(width), _height(height), 
+	StageModel(string name, vector <EntityDef> vEntitiesDef, vector <PersonajeModelo*> vMainCharacters, float itemsPercentage): // unsigned int width, unsigned int height,
+	_name(name), _vEntitiesDef(vEntitiesDef),  _vMainCharacters(vMainCharacters), _itemsPercentage(itemsPercentage) { _tileWidth = DEFAULT_TILE_WIDTH; _tileHeight = DEFAULT_TILE_HEIGHT; firstTile = NULL; this->_tilesMap = NULL; }; //_width(width), _height(height), 
 	StageModel(const StageModel&);
 	StageModel& operator=(const StageModel&);
 	unsigned int width() const; 
 	unsigned int height() const;
 	string name() const; 
 	vector <EntityDef> vEntitiesDef();
-	vector <PersonajeModelo*>* vMainCharacters(); 
+	map <string, string>* StageModel::mapItems();
+	vector <PersonajeModelo*>* vMainCharacters();
+	float itemsPercentage();
 	void width(unsigned); 
 	void height(unsigned);
 	void setSize(unsigned, unsigned);
@@ -40,7 +43,7 @@ public:
 	pair<int,int> pixelToTileCoordinates(pair<int,int> pixelCoordinates);
 	bool isInsideWorld(pair<int,int> tileCoordinates);
 	//Reemplazar luego una por otra
-	void destino(int x,int y,float cameraX,float cameraY);
+	//void destino(int x,int y,float cameraX,float cameraY);
 	std::pair<int, int> destination(int x,int y,float cameraX,float cameraY);
 	//
 	//PersonajeModelo* modelMainCharacters(unsigned );
@@ -58,8 +61,17 @@ public:
 	//void StageModel::resolveBolckedEntities(TileModel* tile);
 	//bool isThereAChar(string &name,int x, int y, float cameraX, float cameraY);
 	void loadNamedChars();
+	void loadMapItems(vector <sItem> vItems);
 	PersonajeModelo* getCharacter(string name);
+	string getItemInfo(string name);
+	//void generateItems(float porcentage);
+	//string manageItemsInitialSynch();
+
 private:
+
+	//void regenerateItem();
+	//Uint32 lastRegeneration;
+	//void reviveItem();
 	string _name;
 	unsigned int _width;
 	unsigned int _height;
@@ -71,6 +83,9 @@ private:
 	vector <TileModel*> tileLevels;
 	vector <PersonajeModelo*> _vMainCharacters;
 	map <string,PersonajeModelo*> mapMainCharacters;
+	map <string,string> _mapItems;
+	float _itemsPercentage;
+	//list <string> itemChanges;
 };
 
 #endif //_STAGEMODEL_H_
